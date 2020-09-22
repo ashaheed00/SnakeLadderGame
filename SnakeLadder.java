48b17a02b3cb0a6b12e8ab51452315edbf273000
ashaheed00 <ashaheed00@gmail.com>
2020-09-22 16:50:51 +0530

    Adding into the UC2 branch

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,7 +2,10 @@ public class SnakeLadder {
 	final static short LADDER = 1;
 	final static short SNAKE = 2;
 	final static int position = 0;
-	
+	static short rollDice;	
+
 	public static void main(String[] args) {
+		rollDice = (short) ((Math.random() * 10 % 6) +1);
+		System.out.println("Player got after rolling the dice: " + rollDice);
 	}
 }
